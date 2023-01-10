9cc08337d18c7cdd4608bfc4b7b64f7453220177
mueiya <mueiya.whatever@gmail.com>
2023-01-10 19:20:25 +0700

    add user and verivy

---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,48 @@
+const {nanoid} = require('nanoid');
+const {Pool} = require('pg');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class UserService {
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async addUser({
+    username,
+    password,
+    fullname,
+  }) {
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO user VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this.pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Failed to add new user');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM user WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this.pool.query(query);
+
+    if (result.rowCount) {
+      throw new InvariantError(`Adding user failed.` +
+        ` ${username} already exists`);
+    }
+  }
+};

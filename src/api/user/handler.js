1a1444977d1b9a99d397f8c28388a718eae5d7d3
mueiya <mueiya.whatever@gmail.com>
2023-01-10 21:10:22 +0700

    adding routes

---
+++ src/api/user/handler.js
@@ -0,0 +1,3 @@
+class userHandler {
+    constructor
+}
---
+++ src/api/user/routes.js
@@ -0,0 +1,14 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/users',
+    handler: handler.postUserHandler,
+  },
+  {
+    method: 'GET',
+    path: '/users/{id}',
+    handler: handler.getUserHandler,
+  },
+];
+
+module.exports = routes;

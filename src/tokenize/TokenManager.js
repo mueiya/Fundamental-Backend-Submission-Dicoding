02ef878f9c2657dc61c03bc69020dd8a99250695
mueiya <mueiya.whatever@gmail.com>
2023-01-12 08:54:16 +0700

    token manager

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -1,7 +1,23 @@
+/* eslint-disable max-len */
 const Jwt = require('@hapi/jwt');
+const InvariantError = require('../exceptions/InvariantError');
 
 const TokenManager = {
-  generateAccessToken: (payload ) =>
-    Jwt.TokenManager.generate(payload, process.env.ACCESS_TOKEN_KEY),
+  generateAccessToken: (payload ) => {
+    Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY);
+  },
+  generateRefreshToken: (payload) => {
+    Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY);
+  },
+  verifyRefreshToken: (payload) => {
+    try {
+      const artifact = Jwt.token.decode(refreshToken);
+      Jwt.token.verifySignature(artifact, process.env.REFRESH_TOKEN_KEY);
+      const {payload} = artifact.decoded;
+      return payload;
+    } catch (error) {
+      throw new InvariantError('Refresh Token is not valid');
+    }
+  },
 };
 module.exports = TokenManager;

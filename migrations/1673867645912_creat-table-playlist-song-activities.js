ef53f49922ede241c0afb600f3921e9626d2b214
mueiya <mueiya.whatever@gmail.com>
2023-01-16 21:37:08 +0700

    adjusting fk for activity

--- migrations/1673867645912_creat-table-playlist-song-activities.js
+++ migrations/1673867645912_creat-table-playlist-song-activities.js
@@ -14,9 +14,11 @@ exports.up = (pgm) => {
     },
     song_id: {
       type: 'VARCHAR(50)',
+      notNull: true,
     },
     user_id: {
       type: 'VARCHAR(50)',
+      notNull: true,
     },
     action: {
       type: 'VARCHAR(50)',
@@ -30,8 +32,6 @@ exports.up = (pgm) => {
 
   // adding fk for playlist_id
   pgm.addConstraint('activities', 'fk_playlist_id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
-  pgm.addConstraint('activities', 'fk_song_id', 'FOREIGN KEY(song_id) REFERENCES songs(id) ON DELETE CASCADE');
-  pgm.addConstraint('activities', 'fk_user_id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
